1c63249f07fd383af1ec21f1c09ea85b6fcc0e97
Ravindra Dhore <ravindrad@ansh.com>
2019-11-26 12:20:29 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'newproject';
+export class AppComponent implements OnInit {
+  name = 'ng2-ckeditor';
+  ckeConfig: any;
+  mycontent: string;
+  log = '';
+  @ViewChild('myckeditor')
+  ckeditor: any;
+
+  constructor() {
+    this.mycontent = `<p>My html content</p>`;
+  }
+
+  ngOnInit() {
+    this.ckeConfig = {
+      allowedContent: false,
+      extraPlugins: 'divarea',
+      forcePasteAsPlainText: true
+    };
+  }
+
+  onChange($event: any): void {
+    console.log(this.mycontent);
+
+    // this.log += new Date() + "<br />";
+  }
+
+  onPaste($event: any): void {
+    console.log('onPaste');
+    // this.log += new Date() + "<br />";
+  }
+
+
+  onEditorChange() {
+    console.log('click');
+  }
+
+  onReady() {
+    console.log('click');
+  }
+
+  onFocus() {
+    console.log('click');
+  }
+
+  onContentDom() {
+    console.log('click');
+  }
+
+  onFileUploadRequest() {
+    console.log('click');
+  }
+
+  onFileUploadResponse() {
+    console.log('click');
+  }
+
+  onBlur() {
+    console.log('click');
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CKEditorModule } from 'ng2-ckeditor';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CKEditorModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
